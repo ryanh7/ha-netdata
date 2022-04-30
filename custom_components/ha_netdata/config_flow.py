d29126feb8ff65b7f120c97541df26da047401fa
ryanh7 <ryanh7@163.com>
2022-04-30 22:13:45 +0800

    添加修改选项

--- custom_components/ha_netdata/config_flow.py
+++ custom_components/ha_netdata/config_flow.py
@@ -1,10 +1,10 @@
 from __future__ import annotations
-from email.policy import default
 import logging
 
 import voluptuous as vol
 from netdata import Netdata
 from homeassistant import config_entries
+from homeassistant.core import callback
 import homeassistant.helpers.config_validation as cv
 from homeassistant.const import CONF_NAME, CONF_HOST, CONF_PORT, CONF_RESOURCES
 
@@ -23,9 +23,11 @@ class NetdataFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
         errors = {}
 
         if user_input is not None:
-            netdata = Netdata(user_input[CONF_HOST], port=user_input[CONF_PORT])
+            netdata = Netdata(user_input[CONF_HOST],
+                              port=user_input[CONF_PORT])
             await netdata.get_allmetrics()
-            self.domains = sorted(list({ m.split(".")[0] for m in netdata.metrics}))
+            self.domains = sorted(
+                list({m.split(".")[0] for m in netdata.metrics}))
             self.metrics = netdata.metrics
             self.config = user_input
             return await self.async_step_domains()
@@ -48,7 +50,7 @@ class NetdataFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
 
         if user_input is not None:
             self.sensors = []
-            for sensor,data in self.metrics.items():
+            for sensor, data in self.metrics.items():
                 if sensor.split(".")[0] not in user_input[CONF_DOMAINS]:
                     continue
                 for element in data["dimensions"].keys():
@@ -66,13 +68,12 @@ class NetdataFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             ),
             errors=errors,
         )
-    
+
     async def async_step_sensors(self, user_input=None):
         """Handle a flow initialized by the user."""
         errors = {}
 
         if user_input is not None:
-            self.filters = sorted(user_input[CONF_RESOURCES])
             self.config.update(user_input)
             await self.async_set_unique_id(
                 f"netdata-{self.config[CONF_HOST]}-{self.config[CONF_PORT]}"
@@ -90,3 +91,64 @@ class NetdataFlowHandler(config_entries.ConfigFlow, domain=DOMAIN):
             errors=errors,
         )
 
+    @staticmethod
+    @callback
+    def async_get_options_flow(config_entry):
+        """Get the options flow for this handler."""
+        return OptionsFlowHandler(config_entry)
+
+
+class OptionsFlowHandler(config_entries.OptionsFlow):
+    def __init__(self, config_entry: config_entries.ConfigEntry) -> None:
+        """Initialize options flow."""
+        self.config_entry = config_entry
+        self.config = dict(config_entry.data)
+
+    async def async_step_init(self, user_input=None):
+        netdata = Netdata(self.config[CONF_HOST], port=self.config[CONF_PORT])
+        await netdata.get_allmetrics()
+        self.domains = sorted(list({m.split(".")[0] for m in netdata.metrics}))
+        self.metrics = netdata.metrics
+
+        if user_input is not None:
+            self.sensors = []
+            for sensor, data in self.metrics.items():
+                if sensor.split(".")[0] not in user_input[CONF_DOMAINS]:
+                    continue
+                for element in data["dimensions"].keys():
+                    self.sensors.append(f"{sensor}/{element}")
+            self.sensors = sorted(self.sensors)
+            self.config.update(user_input)
+            return await self.async_step_sensors()
+
+        return self.async_show_form(
+            step_id="init",
+            data_schema=vol.Schema(
+                    {
+                        vol.Required(CONF_DOMAINS, default=self.config[CONF_DOMAINS]): cv.multi_select(self.domains)
+                    }
+            ),
+        )
+
+    async def async_step_sensors(self, user_input=None):
+        """Handle a flow initialized by the user."""
+        errors = {}
+
+        if user_input is not None:
+            self.config.update(user_input)
+            self.hass.config_entries.async_update_entry(
+                self.config_entry,
+                data=self.config
+            )
+            await self.hass.config_entries.async_reload(self.config_entry.entry_id)
+            return self.async_create_entry(title="", data=self.config)
+
+        return self.async_show_form(
+            step_id="sensors",
+            data_schema=vol.Schema(
+                    {
+                        vol.Required(CONF_RESOURCES, default=self.config[CONF_RESOURCES]): cv.multi_select(self.sensors)
+                    }
+            ),
+            errors=errors,
+        )
